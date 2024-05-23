1b442c3d75c2b13861db7661a35c98b0bef29977
Rafael <ralfrr55@gmail.com>
2024-05-23 14:30:42 -0300

    kafka envio de mensagem

---
+++ kafka/consumidor.py
@@ -0,0 +1,13 @@
+# /kafka/bin$ ./kafka-console-consumer.sh --bootstrap-server 127.0.0.1:9092 --topic mensagens --from-beginning
+
+from kafka import KafkaConsumer as kc
+consumidor = kc("mensagens", bootstrap_servers="127.0.0.1:9092", consumer_timeout_ms=1000,
+               group_id="consumidores")
+
+for mensagem in consumidor:
+    print("Topic: ", mensagem.topic)
+    print("Partição ", mensagem.partition)
+    print("Chave", mensagem.key)
+    print("offset ", mensagem.offset)
+    print("Mensagem ", mensagem.value)
+    print("-------------------------------------")
---
+++ kafka/produtor.py
@@ -0,0 +1,10 @@
+# /kafka/bin$ ./kafka-server-start.sh ../config/server.properties
+# /kafka/bin$ ./zookeeper-server-start.sh ../config/zookeeper.properties
+
+from kafka import KafkaProducer as kp
+import random
+produtor = kp(bootstrap_servers="127.0.0.1:9092")
+
+for x in range(10):
+    n = random.random()
+    produtor.send("mensagens", key=b"Chave %d" % x, value=b"Mensagem %f " % n )
